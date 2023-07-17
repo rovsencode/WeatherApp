96fd3db037d2b1096289627ab3ee212aff7dd9ee
rovsencode <rovshanakh@code.edu.az>
2023-07-17 23:02:38 +0400

    add Weather information and it's styles

--- src/Components/Weather/index.js
+++ src/Components/Weather/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import backgroundImage from "../Weather/sky.jpg";
 import "../Weather/index.scss";
+import WbSunnyRoundedIcon from "@mui/icons-material/WbSunnyRounded";
 function Weather() {
   return (
     <div
@@ -12,7 +13,26 @@ function Weather() {
     >
       <h5 className="weather-title">Weather App</h5>
       <div className="searchArea">
-        <input type="text" className="searchInput" placeholder="Enter a City..." />
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Enter a City..."
+        />
+      </div>
+      <div className="container information-cr">
+        <div className="row information-row">
+          <div className="weatherInformation">
+            <div className="weatherIcon">
+              <WbSunnyRoundedIcon htmlColor="orange" sx={{ fontSize: 150 }} />
+            </div>
+            <div className="information">
+              <p className="day">Today</p>
+              <h2>Baku</h2>
+              <p className="temparature">Temparature: 17 C </p>
+              <span style={{ color: "white" }}>clear sky</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
